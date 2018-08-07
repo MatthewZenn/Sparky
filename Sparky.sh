7e10dc7277f8934a673985c15434df9e245163f8
Matthew Zenn <spyguy57@gmail.com>
2018-08-06 20:03:24 -0400

    Update Sparky.sh
    made it colorful!

--- Sparky.sh
+++ Sparky.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 # Sparky
 clear
+echo ""
 echo " ███████╗██████╗  █████╗ ██████╗ ██╗  ██╗██╗   ██╗"
 echo " ██╔════╝██╔══██╗██╔══██╗██╔══██╗██║ ██╔╝╚██╗ ██╔╝"
 echo " ███████╗██████╔╝███████║██████╔╝█████╔╝  ╚████╔╝ "
 echo " ╚════██║██╔═══╝ ██╔══██║██╔══██╗██╔═██╗   ╚██╔╝  "
 echo " ███████║██║     ██║  ██║██║  ██║██║  ██╗   ██║   "
 echo " ╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   "
-echo "                     FOR LINUX"
-echo "__________________________________________________"
+/bin/echo -e "\e[1;5;95m                    FOR LINUX                     \e[0m"
+/bin/echo -e "\e[1m _________________________________________________\e[0m"
 echo ""
-printf " "%s"" Current_User:
+printf " "%s"\033[1;91mCurrent_User:\033[0m\n"
 id -un
-printf " "%s"" Device_Manufacturer: 
+printf " "%s"\033[1;91mDevice_Manufacturer:\033[0m\n" 
 cat /sys/devices/virtual/dmi/id/sys_vendor
-printf " "%s"" OS_Type: 
+printf " "%s"\033[1;91mOS_Type:\033[0m\n" 
 uname
-printf " "%s"" OS_Version: 
+printf " "%s"\033[1;91mOS_Version:\033[0m\n"
 cat /sys/devices/virtual/dmi/id/bios_version
-printf " "%s"" IP_Address: 
+printf " "%s"\033[1;91mIP_Address:\033[0m\n" 
 ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'
 printf ""%s""
 echo ""
